18e9e0b77c37bd984a7ddca8aba6034915f19e4c
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2018-12-09 23:03:53 +0200

    Moved init of keyspace and basic tables from server to cassandra-init.sh script, ovverided cassandra containers command option, ensured connection of server to databse

---
+++ warehouse/cassandra-init.sh
@@ -0,0 +1,34 @@
+KEYSPACE="delivery_management"
+REPLICATION_FACTOR=3
+TABLE="orders"
+CQL="CREATE KEYSPACE IF NOT EXISTS $KEYSPACE WITH replication = {'class' : 'SimpleStrategy','replication_factor' : $REPLICATION_FACTOR};
+     CREATE TABLE IF NOT EXISTS $KEYSPACE.$TABLE (
+        order_id text,
+     		awb_number text,
+     		allow_open_parcel text,
+     		created_date timestamp,
+     		labels list<text>,
+     		latitude float,
+     		longitude float,
+     		service_payment float,
+     		receiver_address text,
+     		receiver_address_locality text,
+     		receiver_contact text,
+     		receiver_name text,
+     		receiver_phone text,
+     		shipper_address text,
+     		shipper_address_locality text,
+     		shipper_contact text,
+     		shipper_name text,
+     		shipper_phone text,
+     		status_group_id int,
+     		todayImportant boolean,
+     		PRIMARY KEY ((order_id, awb_number), created_date));"
+
+until echo $CQL | cqlsh
+ do
+   echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
+   sleep 2
+ done &
+
+exec /docker-entrypoint.sh "$@"
--- warehouse/db/cassandra.go
+++ warehouse/db/cassandra.go
@@ -6,9 +6,8 @@ import (
 )
 
 const (
-	KEYSPACE = "DeliveryManagement"
+	KEYSPACE = "delivery_management"
 	HOST 	 = "cassandraSeed"
-	REPLICATION_FACTOR = 3
 )
 
 type Cassandra struct {
@@ -20,6 +19,7 @@ func (db *Cassandra) ConnectToCluster() {
 	// connect to the cluster
 	db.cluster = gocql.NewCluster(HOST)
 	db.cluster.Port = 9042
+	db.cluster.Keyspace = KEYSPACE
 	db.cluster.Consistency = gocql.Quorum
 	db.session, _ = db.cluster.CreateSession()
 	defer db.session.Close()
@@ -35,52 +35,6 @@ func (db *Cassandra) initSession() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		db.initKeySpace()
-		db.initTables()
 		fmt.Println("Connected to Cassandra! Init done!")
-	}
-
-}
-
-func (db *Cassandra) initKeySpace() {
-	err := db.session.Query(fmt.Sprintf(`CREATE KEYSPACE IF NOT EXISTS %s 
-	WITH replication = {
-		'class' : 'SimpleStrategy',
-		'replication_factor' : %d
-	}`, KEYSPACE, REPLICATION_FACTOR)).Exec()
-
-	if err != nil {
-		fmt.Println("Error while creating keyspace")
-		fmt.Println(err)
-	}
-}
-
-func (db *Cassandra) initTables() {
-	err := db.session.Query(`CREATE TABLE IF NOT EXISTS orders(
-		order_id text,
-		awb_number text,
-		allow_open_parcel text,
-		created_date timestamp,
-		labels list<text>,
-		latitude float,
-		longitude float,
-		service_payment float,
-		receiver_address text,
-		receiver_address_locality text,
-		receiver_contact text,
-		receiver_name text,
-		receiver_phone text,
-		shipper_address text,
-		shipper_address_locality text,
-		shipper_contact text,
-		shipper_name text,
-		shipper_phone text,
-		status_group_id int,
-		todayImportant boolean,
-		PRIMARY KEY ((order_id, awb_number), created_date))`).Exec()
-
-	if err != nil {
-		fmt.Println("Error while creating tables")
-		fmt.Println(err)
 	}
 }
